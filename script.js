f01dced29db43bef3f6b9036ee3a7661857e6341
ryi2526 <47389471+ryi2526@users.noreply.github.com>
2019-07-11 21:24:00 +0900

    016

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let localStream = null;
 let peer = null;
 let existingCall = null;
 
-navigator.mediaDevices.getUserMedia({video: true, audio: true});
+navigator.mediaDevices.getUserMedia({video: true, audio: true})
 
 peer = new Peer({
     key: 'b1e7600c-ab47-4d54-b497-df1ea1c937f2',
