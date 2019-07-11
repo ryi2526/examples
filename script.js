5ef024060f11d41147a6196e28176dbfdec79877
ryi2526 <47389471+ryi2526@users.noreply.github.com>
2019-07-11 21:09:42 +0900

    013

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let localStream = null;
 let peer = null;
 let existingCall = null;
 
-"window.__SKYWAY_KEY__ = 'b1e7600c-ab47-4d54-b497-df1ea1c937f2';"
+window.__SKYWAY_KEY__ = 'b1e7600c-ab47-4d54-b497-df1ea1c937f2';
 
 //navigator.mediaDevices.getUserMedia({video: true, audio: true})
     // 省略
